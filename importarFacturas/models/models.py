7da00d438f3ce0633d6fb4b1f72d23722500b918
stefanjaw <stefanj@avalantec.com>
2020-07-24 13:51:36 -0600

    modified from account.invoice to account.move

--- importarFacturas/models/models.py
+++ importarFacturas/models/models.py
@@ -2,7 +2,7 @@
 
 from odoo import models, fields, api
 class Invoice(models.Model):
-    _inherit = "account.invoice"
+    _inherit = "account.move"
     clave_proveedor = fields.Char(string="Clave Proveedor", )
     numero_consecutivo_aceptacion = fields.Char(string="Numero Consecutivo Aceptación", )
 
